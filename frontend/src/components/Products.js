f0e9764d82a21fa8459daee35596f07175ede280
Andres Hoyos Garcia <andyholesdev@gmail.com>
2023-11-13 11:05:51 -0300

    add loading image

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -5,10 +5,12 @@ import Card from 'react-bootstrap/Card';
 
 function Products({onProductSelect}) {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState([]);
 
     useEffect(() => {
       axios.get(process.env.REACT_APP_BASE_URL+'/products')
         .then((response) => {
+          setLoading(false)
           setProducts(response.data);
         })
         .catch((error) => {
@@ -21,31 +23,35 @@ function Products({onProductSelect}) {
       };
 
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id} className="d-inline-flex">
-        <Card className="d-inline-flex p-4 m-2" style={{width:'20em'}}>
-          <Card.Img
-            variant="top"
-            src={"data:image/octet-stream;base64," + product.image}
-            style={{height: '15em', objectFit:'contain'}}
-            />
-          <Card.Body>
-            <Card.Title>{product.name}</Card.Title>
-            <Card.Text>
-              {"$" + product.price.toLocaleString('de-DE')}
-            </Card.Text>
-            <Button
-              variant="dark"
-              onClick={() => handleProductSelect(product.id)}
-            >
-              Agregar al carrito
-            </Button>
-          </Card.Body>
-        </Card>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id} className="d-inline-flex">
+          <Card className="d-inline-flex p-4 m-2" style={{width:'20em'}}>
+            <Card.Img
+              variant="top"
+              src={"data:image/octet-stream;base64," + product.image}
+              style={{height: '15em', objectFit:'contain'}}
+              />
+            <Card.Body>
+              <Card.Title>{product.name}</Card.Title>
+              <Card.Text>
+                {"$" + product.price.toLocaleString('de-DE')}
+              </Card.Text>
+              <Button
+                variant="dark"
+                onClick={() => handleProductSelect(product.id)}
+              >
+                Agregar al carrito
+              </Button>
+            </Card.Body>
+          </Card>
+          </li>
+        ))}
+      </ul>
+      {loading && <img src="https://i.gifer.com/ZKZg.gif" 
+      style={{height:'7em', position:'absolute', top:'45%'}}/>}
+    </>
   );
 }
 
